16fa418a250a0ca6373294a5abc6d5bbd59ceb54
Brian Johnson <brianjohnsona@yahoo.com>
2019-06-15 16:34:32 +0000

    Added outputs for devise users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  config.ation_mailer.default_url_options = { host: 'nomster-brian-johnson.herokuapp.com' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
