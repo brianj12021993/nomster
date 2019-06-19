7f50918b63645066e8de751164ad25077edb97e2
Brian Johnson <brianjohnsona@yahoo.com>
2019-06-19 23:20:51 +0000

    Trying to fix heroku

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,17 +4,8 @@ class Place < ApplicationRecord
 	after_validation :geocode 
 
 	validates :name, presence: true, length: { minimum: 3 }
+	validates :address, presence: true
+	validates :description, presence: true
 end
 
-place = Place.new(name: "New Place")
-place.valid? # => true
-place.errors[:name] # => []
- 
-place = Place.new(name: "NP")
-place.valid? # => false
-place.errors[:name] # => ["is too short (minimum is 3 characters)"]
- 
-place = Place.new
-place.valid? # => false
-place.errors[:name]
- # => ["can't be blank", "is too short (minimum is 3 characters)"]
+
